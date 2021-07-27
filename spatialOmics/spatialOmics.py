8c62e28f1dc113ce3d4bfcdccbc39d1425b75da9
Adriano Martinelli <art@zurich.ibm.com>
2021-07-27 18:48:20 +0200

    implement to_h5py, from_h5py

--- spatialOmics/spatialOmics.py
+++ spatialOmics/spatialOmics.py
@@ -1,18 +1,15 @@
 import pandas as pd
-import numpy as np
 import networkx as nx
 from skimage import io
-import time
 
 import os
 import copy
 
-# from tqdm import tqdm
-import pickle
 import h5py
 
+
 # %%
-class SpatialOmics():
+class SpatialOmics:
 
     def __init__(self):
 
@@ -104,15 +101,71 @@ class SpatialOmics():
     def __len__(self):
         return len(self.spl)
 
-    def to_h5py(self, file=None):
+    def to_h5py(self, file: str = None) -> None:
+        """
+
+        Args:
+            file: file to write to, defaults to self.h5py_file
+
+        Returns:
+
+        """
         if file is None:
             file = self.h5py_file
 
-        print(f'File `{os.path.basename(file)}` save at {os.path.dirname(file)}')
+        with h5py.File(file, mode='w') as f:
+            f.create_dataset('h5py_file', data=self.h5py_file)
+
+            # obs
+            for spl in self.obs:
+                # use pandas function
+                self.obs[spl].to_hdf(file, f'obs/{spl}')
+
+            # obsm
+            for spl in self.obsm:
+                f.create_dataset(f'obsm/{spl}', data=self.obsm[spl])
+
+            # spl
+            self.spl.to_hdf(file, 'spl')
+
+            # var
+            for spl in self.var:
+                # use pandas function
+                self.var[spl].to_hdf(file, f'var/{spl}')
+
+            # X
+            for spl in self.X:
+                # use pandas function
+                self.obs[spl].to_hdf(file, f'X/{spl}')
+
+            # uns
+            # TODO: currently we do not support storing uns to h5py due to datatype restrictions
+
+            # G
+            for spl in self.G:
+                for key in self.G[spl]:
+                    g = self.G[spl][key]
+                    df = nx.to_pandas_edgelist(g)
+
+                    # use pandas function
+                    df.to_hdf(file, f'G/{spl}/{key}')
+
+            # images
+            for spl in self.images:
+                img = self.images[spl]
+                f.create_dataset(f'images/{spl}', data=img)
+
+            # masks
+            for spl in self.masks:
+                for key in self.masks[spl].keys():
+                    msk = self.masks[spl][key]
+                    f.create_dataset(f'masks/{spl}/{key}', data=msk)
+
+        print(f'File `{os.path.basename(file)}` saved to {os.path.abspath(file)}')
         print(f'File size: {os.path.getsize(file) / (1024 * 1024):.2f} MB')
 
     @classmethod
-    def form_h5py(cls, file=None, include_images=False, include_mask=False):
+    def from_h5py(cls, file=None):
         """
 
         Args:
@@ -126,66 +179,82 @@ class SpatialOmics():
         """
 
         so = SpatialOmics()
-        # all class attributes
-        attrs = ['graph_engine', 'random_seed', 'pickle_file', 'h5py_file', 'obs', 'spl', 'var', 'X', 'G', 'uns', 'spl_keys', 'images', 'masks']
 
-        # sample-level attributes
-        spl_attrs = ['G', 'X', 'images', 'masks', 'obs', 'var']
-        pd_attrs = ['X', 'obs', 'var']  # panda dataframes
-        nx_attrs = ['G']  # networkx graphs
-        uns_attrs = ['masks', 'uns']
         with h5py.File(file, 'r') as f:
-            for attr in attrs:
-                if attr not in f:
-                    continue
-                if attr == 'masks' and include_mask is False:
-                    continue
-                if attr == 'images' and include_images is False:
-                    continue
+            so.h5py_file = str(f['h5py_file'][...])
 
-                if attr in spl_attrs:
-                    spls = f[attr].keys()
-                    tmp = {}
-                    for spl in spls:
-                        if attr in pd_attrs:
-                            tmp.update({spl:pd.read_hdf(file, f'{attr}/{spl}/')})
-                        elif attr in nx_attrs:
-                            # TODO: For the reconsturction of the graphs we need to save the observation ids as well otherwise we start at 0
-                            m = f[attr][spl][spl][...]
-                            tmp.update({spl:nx.from_numpy_array(m)})
-                        elif attr in uns_attrs:
-                            key_item = {}
-                            for key in f[attr][spl].keys():
-                                m = f[attr][spl][key][spl][...]
-                                key_item.update({key: m})
-                            tmp.update({spl: key_item})
-                        else:
-                            m = f[attr][spl][spl][...]
-                            tmp.update({spl:m})
-                    setattr(so, attr, tmp)
-                elif attr == 'spl':
-                    tmp = pd.read_hdf(file, f'{attr}/')
-                    setattr(so, attr, tmp)
-                else:
-                    m = f[attr][...]
-                    if attr == 'random_seed':
-                        setattr(so, attr, int(m))
-                    else:
-                        setattr(so, attr, m)
+            # obs
+            if 'obs' in f:
+                for spl in f['obs'].keys():
+                    so.obs[spl] = pd.read_hdf(file, f'obs/{spl}')
+
+            # obsm
+            if 'obsm' in f:
+                for spl in f['obsm'].keys():
+                    so.obsm[spl] = f[f'obsm/{spl}'][...]
+
+            # spl
+            so.spl = pd.read_hdf(file, 'spl')
+
+            # var
+            if 'var' in f:
+                for spl in f['var'].keys():
+                    so.var[spl] = pd.read_hdf(file, f'var/{spl}')
+
+            # X
+            if 'X' in f:
+                for spl in f['X'].keys():
+                    so.X[spl] = pd.read_hdf(file, f'X/{spl}')
+
+            # G
+            if 'G' in f:
+                for spl in f['G'].keys():
+                    for key in spl:
+                        if spl not in so.G:
+                            so.G[spl] = {}
+                        so.G[spl][key] = nx.from_pandas_edgelist(pd.read_hdf(file, f'G/{spl}/{key}'))
+
+            # images
+            if 'images' in f:
+                for spl in f['images'].keys():
+                    if spl not in so.images:
+                        so.images[spl] = {}
+                    so.images[spl] = f[f'images/{spl}'][...]
+
+            # masks
+            if 'masks' in f:
+                for spl in f['masks'].keys():
+                    for key in f[f'masks/{spl}']:
+                        if spl not in so.masks:
+                            so.masks[spl] = {}
+                        so.masks[spl][key] = f[f'masks/{spl}/{key}'][...]
+
         return so
 
-    def to_pickle(self, file=None):
-        if file is None:
-            file = self.pickle_file
+    def to_pickle(self, file: str = None) -> None:
+        """Save spatialOmics instance to pickle.
 
-        print(f'File `{os.path.basename(file)}` save at {os.path.dirname(file)}')
-        print(f'File size: {os.path.getsize(file) / (1024 * 1024):.2f} MB')
+        Args:
+            file: file to which instance is saved.
+
+        Returns:
+
+        """
+        raise NotImplementedError('This version does not yet support saving pickled instances')
 
     @classmethod
-    def from_pickle(cls, file=None):
-        with open(file, 'rb') as f:
-            so = pickle.load(f)
-        return so
+    def from_pickle(cls, file: str = None)\
+            :
+        """Load spatialOmics instance from pickled file.
+
+        Args:
+            file: file to un-pickle
+
+        Returns:
+            spatialOmics instance
+
+        """
+        raise NotImplementedError('This version does not yet support reading pickled instances')
 
     def copy(self):
         """copy IMCData without copying graphs, masks and tiffstacks"""
